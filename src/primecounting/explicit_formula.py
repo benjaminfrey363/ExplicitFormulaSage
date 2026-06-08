89eeafdf9a7d73cd11ce99ab93062f9a01229669
Ben Frey <benjamin.frey@ucalgary.ca>
2026-06-08 14:31:59 -0600

    explicit formula beginning

--- src/primecounting/explicit_formula.py
+++ src/primecounting/explicit_formula.py
@@ -37,3 +37,95 @@ def _as_complex(z,prec=80):
     '''
     C = ComplexField(prec)
     return C(z)
+
+
+def _validate_x(x):
+    '''
+    validate x as an argument for explicit formula
+    '''
+    if x <= 1:
+        raise ValueError("x must be greater than 1")
+    
+
+def zero_from_ordinate(gamma, prec=80):
+    '''
+    Giving an ordinate gamma, return complex number with specified
+    precision with real part 1/2 and imaginary part gamma
+    '''
+    C = ComplexField(prec)
+    R = RealField(prec)
+    return C(R(1) / R(2), R(gamma))
+
+
+def single_zero_term(x, gamma, prec=80):
+    '''
+    Compute x^rho / rho for rho = 1/2 + i gamma
+    '''
+
+    _validate_x(x)
+
+    R = RealField(prec)
+    C = ComplexField(prec)
+
+    x = R(x)
+    rho = zero_from_ordinate(gamma, prec=prec)
+
+    return C(x) ** rho / rho
+
+
+def positive_zero_sum(x, gammas, prec=80):
+    """
+    Compute the positive ordinate zero sum
+
+        sum_{gamma > 0} x^(1/2 + i gamma)/(1/2 + i gamma)
+
+    over the supplied list of positive zero ordinates.
+    """
+    _validate_x(x)
+
+    C = ComplexField(prec)
+    total = C(0)
+
+    for gamma in gammas:
+        total += single_zero_term(x, gamma, prec=prec)
+
+    return total
+
+
+def zero_contribution_assuming_rh(x, gammas, prec=80):
+    """
+    Compute the symmetric zero contribution assuming RH:
+
+        2 Re sum_{gamma > 0} x^(1/2 + i gamma)/(1/2 + i gamma).
+    """
+    S = positive_zero_sum(x, gammas, prec=prec)
+    return 2 * S.real()
+
+
+def explicit_formula_correction(x, prec=80):
+    """
+    Compute the nonzero correction term
+
+        log(2*pi) + (1/2) log(1 - x^(-2)).
+
+    In the explicit formula this is subtracted from x together with
+    the zero contribution.
+
+    Parameters
+    ----------
+    x : real
+        Point at which the explicit formula is evaluated.
+    prec : int
+        Working precision in bits.
+
+    Returns
+    -------
+    Sage real number
+        The correction term.
+    """
+    _validate_x(x)
+
+    R = RealField(prec)
+    x = R(x)
+
+    return log(2 * R(pi)) + (R(1) / R(2)) * log(1 - x ** (-2))
